96069b6499568e3d44fbf5ed01cc98132fe03a06
JeffKatzy <jeffkatzygit config --global user.name JeffKatzy>
2022-01-20 16:10:00 -0500

    update orm setup

--- orm.py
+++ orm.py
@@ -1,14 +1,3 @@
-def build_from_record(Class, record):
-    if not record: return None
-
-    attr = dict(zip(Class.attributes, record))
-    obj = Class()
-    obj.__dict__ = attr
-    return obj
-
-def build_from_records(Class, records):
-    return [build_from_record(Class, record) for record in records]
-
 
 def values(obj):
     venue_attrs = obj.__dict__
